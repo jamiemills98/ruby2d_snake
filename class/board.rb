8d239e91d1318eaec9c1d3553617c8678eb65190
jamiemills98 <jamiemills98@hotmail.com>
2019-06-03 11:44:51 +1000

    didn't fix boardf

--- class/board.rb
+++ class/board.rb
@@ -1,29 +1,29 @@
 class Board
-    attr_accessor :board, :score
-    def initilize
-        @score = 0
-        @board = Array.new
-        board[0] = "---------------------------------------------------------------"
-        board[1] = "|                                                             |"
-        board[2] = "|                                                             |"
-        board[3] = "|                                                             |"
-        board[4] = "|                                                             |"
-        board[5] = "|                                                             |"
-        board[6] = "|                                                             |"
-        board[7] = "|                                                             |"
-        board[8] = "|                                                             |"
-        board[9] = "|                                                             |"
-        board[10] = "---------------------------------------------------------------"
+    attr_accessor :board, :score, 
+    def initilize (input = {})
+    @score = 0
+    @board = Array.new
+    board[0] = ["---------------------------------------------------------------"]
+    @board[1] = ["|                                                             |"]
+    @board[2] = ["|                                                             |"]
+    @board[3] = ["|                                                             |"]
+    @board[4] = ["|                                                             |"]
+    @board[5] = ["|                                                             |"]
+    @board[6] = ["|                                                             |"]
+    @board[7] = ["|                                                             |"]
+    @board[8] = ["|                                                             |"]
+    @board[9] = ["|                                                             |"]
+    @board[10] = ["---------------------------------------------------------------"]
     end
 
     def display
         puts "\e[2J\e[f"
         puts @board.map { |x| x.join(' ') }
-        puts "Score: #{@score}"
-        puts
-        puts "Press 'q' to quit"
+        # puts "Score: #{@score}"
+        # puts
+        # puts "Press 'q' to quit"
     end
 end
 
 board = Board.new
-puts display(board)
+puts board.display
---
+++ main.rb
@@ -0,0 +1,3 @@
+require_relative 'class/board'
+
+# board = Board.new
