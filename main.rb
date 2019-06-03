df930012e6753dc4f95924c0fd73b398fc977fed
jamiemills98 <jamiemills98@hotmail.com>
2019-06-03 20:12:56 +1000

    gem install ruby2d, also make background and snake that constantly moves

--- main.rb
+++ main.rb
@@ -1,3 +1,60 @@
-require_relative 'class/board'
+require 'ruby2d'
 
-# board = Board.new
+set background: 'teal'
+set fps_cap: 20
+
+GRID_SIZE = 20
+
+
+class Snake
+    attr_writer :direction
+    def initialize 
+       @positions = [[2,0], [2,1], [2,2], [2,3]]
+       @direction = 'down'
+    end
+
+    def draw
+        @positions.each do |position|
+            Square.new(x: position[0]* GRID_SIZE, y: position[1] * GRID_SIZE, size: GRID_SIZE - 1, color: 'white')
+        end
+    end
+
+    def move
+        @positions.shift
+        case @direction
+        when 'down'
+            @positions.push([head[0], head[1] + 1])
+        when 'up'
+            @positions.push([head[0], head[1] - 1])
+        when 'left'
+            @positions.push([head[0] - 1, head[1]])
+        when 'right'
+            @positions.push([head[0] + 1, head[1]])
+        end
+    end
+
+    private
+
+    def head 
+        @positions.last
+    end
+end
+
+
+snake = Snake.new
+
+
+update do 
+    clear
+    
+    snake.move
+    snake.draw
+end
+
+on :key_down do |event|
+    if ['up', 'down', 'left', 'right'].include?(event.key)
+        snake.direction = event.key
+    end
+end
+
+show
