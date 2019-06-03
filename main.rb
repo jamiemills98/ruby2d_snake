39bbff77e5f57ecef01400a4333d6c63bfbc06ba
jamiemills98 <jamiemills98@hotmail.com>
2019-06-03 20:38:47 +1000

    score adding

--- main.rb
+++ main.rb
@@ -4,7 +4,8 @@ set background: 'teal'
 set fps_cap: 20
 
 GRID_SIZE = 20
-
+GRID_WIDTH = Window.width / GRID_SIZE
+GRID_HEIGHT = Window.height / GRID_SIZE
 
 class Snake
     attr_writer :direction
@@ -23,37 +24,86 @@ class Snake
         @positions.shift
         case @direction
         when 'down'
-            @positions.push([head[0], head[1] + 1])
+            @positions.push(new_coords(head[0], head[1] + 1))
         when 'up'
-            @positions.push([head[0], head[1] - 1])
+            @positions.push(new_coords(head[0], head[1] - 1))
         when 'left'
-            @positions.push([head[0] - 1, head[1]])
+            @positions.push(new_coords(head[0] - 1, head[1]))
         when 'right'
-            @positions.push([head[0] + 1, head[1]])
+            @positions.push(new_coords(head[0] + 1, head[1]))
         end
     end
 
+    def can_change_direction_to?(new_direction)
+        case @direction
+        when 'up' then new_direction != 'down'
+        when 'down' then new_direction != 'up'
+        when 'left' then new_direction != 'right'
+        when 'right' then new_direction != 'left'
+        end
+    end
+
+    def x 
+        head[0]
+    end
+
+    def y 
+        head[1]
+    end
     private
 
+    def new_coords(x, y)
+        [x % GRID_WIDTH, y % GRID_HEIGHT]
+    end
     def head 
         @positions.last
     end
 end
 
+class Game
+    def initialize
+        @score = 0
+        @ball_x = rand(GRID_WIDTH)
+        @ball_y = rand(GRID_HEIGHT)
+    end
+    def draw
+            Square.new(x: @ball_x * GRID_SIZE, y: @ball_y * GRID_SIZE, size: GRID_SIZE, color: 'fuchsia')
+            Text.new("Score: #{@score}", color:"black", x: 10, y: 10, size:25)
+    end
+    
+    def snake_hit_ball?(x, y)
+        @ball_x == x && @ball_y == y
+    end
+
+    def record_hit
+        @score += 1
+        @ball_x = rand(GRID_WIDTH)
+        @ball_y = rand(GRID_HEIGHT)
+    end
+end
+
+
 
 snake = Snake.new
-
+game = Game.new
 
 update do 
     clear
     
     snake.move
     snake.draw
+    game.draw
+
+    if game.snake_hit_ball?(snake.x, snake.y)
+        game.record_hit
+    end
 end
 
 on :key_down do |event|
     if ['up', 'down', 'left', 'right'].include?(event.key)
+        if snake.can_change_direction_to?(event.key)
         snake.direction = event.key
+        end
     end
 end
 
